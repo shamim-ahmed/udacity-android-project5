60a9d5a5db8ee9f4d930cb97245a98c313e39cfa
Shamim Ahmed <shamim.buet.99@gmail.com>
2016-03-06 12:25:05 +1100

    now the loading indicator is rendered correctly for swipe refresh layout

--- XYZReader/src/main/java/edu/udacity/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/edu/udacity/xyzreader/ui/ArticleListActivity.java
@@ -47,6 +47,8 @@ public class ArticleListActivity extends ActionBarActivity implements
         final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        int offsetFromTop = 250;
+        mSwipeRefreshLayout.setProgressViewOffset(false, 0, offsetFromTop);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         getLoaderManager().initLoader(0, null, this);
