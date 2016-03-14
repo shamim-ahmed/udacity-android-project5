6fa9e9921ff4b38e3d87b5a573a2f2106265532f
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-03-15 09:12:19 +1100

    now the app title is displayed in article detail screen

--- XYZReader/src/main/java/edu/udacity/xyzreader/ui/ArticleDetailActivity.java
+++ XYZReader/src/main/java/edu/udacity/xyzreader/ui/ArticleDetailActivity.java
@@ -1,7 +1,9 @@
 package edu.udacity.xyzreader.ui;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import edu.udacity.xyzreader.R;
 
@@ -14,7 +16,14 @@ public class ArticleDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_article_detail);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+        }
     }
 }
