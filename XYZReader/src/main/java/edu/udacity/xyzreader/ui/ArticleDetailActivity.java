cdbe29a454a6e4498dacca8cf10ed832bd430e46
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-03-16 08:15:34 +1100

    now the article can be successfully retrieved in article detail view

--- XYZReader/src/main/java/edu/udacity/xyzreader/ui/ArticleDetailActivity.java
+++ XYZReader/src/main/java/edu/udacity/xyzreader/ui/ArticleDetailActivity.java
@@ -1,17 +1,23 @@
 package edu.udacity.xyzreader.ui;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import edu.udacity.xyzreader.R;
+import edu.udacity.xyzreader.data.ItemsContract;
 
 /**
  * An activity representing a single Article detail screen, letting you swipe between articles.
  */
 public class ArticleDetailActivity extends AppCompatActivity {
 
+    private static final String TAG = ArticleDetailActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,37 @@ public class ArticleDetailActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDefaultDisplayHomeAsUpEnabled(true);
         }
+
+        // find the item
+        Uri articleUri = getIntent().getData();
+        Log.i(TAG, String.format("The item URI is : %s", articleUri));
+
+        if (articleUri != null) {
+            Cursor cursor = getContentResolver().query(articleUri, null, null, null, null);
+
+            if (cursor != null) {
+                cursor.moveToFirst();
+                ContentValues values = readValues(cursor);
+                Log.i(TAG, "The photo url is " + values.get(ItemsContract.Items.PHOTO_URL));
+                cursor.close();
+            }
+        }
+    }
+
+    private ContentValues readValues(Cursor cursor) {
+        int titleIndex = cursor.getColumnIndex(ItemsContract.Items.TITLE);
+        int photoUrlIndex = cursor.getColumnIndex(ItemsContract.Items.PHOTO_URL);
+        int bodyIndex = cursor.getColumnIndex(ItemsContract.Items.BODY);
+
+        String title = cursor.getString(titleIndex);
+        String photoUrl = cursor.getString(photoUrlIndex);
+        String body = cursor.getString(bodyIndex);
+
+        ContentValues values = new ContentValues();
+        values.put(ItemsContract.Items.TITLE, title);
+        values.put(ItemsContract.Items.PHOTO_URL, photoUrl);
+        values.put(ItemsContract.Items.BODY, body);
+
+        return values;
     }
 }
