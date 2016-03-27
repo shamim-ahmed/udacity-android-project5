03c6fbccd03def9a56e059dd1a5342086288713d
Shamim Ahmed <shamim.buet.99@gmail.com>
2016-03-27 13:49:11 +1100

    removed an unused import

--- XYZReader/src/main/java/edu/udacity/xyzreader/ui/ArticleDetailActivity.java
+++ XYZReader/src/main/java/edu/udacity/xyzreader/ui/ArticleDetailActivity.java
@@ -13,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
